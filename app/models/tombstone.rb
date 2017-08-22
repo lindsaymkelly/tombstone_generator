df54ae6dbc4547eed9fb65233964c51f7ef8dd6e
lindsaymkelly <lindzlady@gmail.com>
2017-08-22 12:58:01 -0400

    Configure cloudinary

--- app/models/tombstone.rb
+++ app/models/tombstone.rb
@@ -1,8 +1,8 @@
 class Tombstone < ApplicationRecord
   validates_presence_of :acquired_text, :year, :conducted_text, :representation
 
-  has_attached_file :acquiror_logo, styles: { medium: "380x150>", thumb: "100x100>" }
-  has_attached_file :acquired_logo, styles: { medium: "380x100>", thumb: "100x100>" }
+  has_attached_file :acquiror_logo, :storage => :cloudinary, :path => ':id/:style/:filename', styles: { medium: "380x150>", thumb: "100x100>" }
+  has_attached_file :acquired_logo, :storage => :cloudinary, :path => ':id/:style/:filename', styles: { medium: "380x100>", thumb: "100x100>" }
 
   validates_attachment_content_type :acquiror_logo, :acquired_logo, content_type: /\Aimage\/.*\z/
 
